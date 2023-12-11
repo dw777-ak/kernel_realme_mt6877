59eae5fc7bfbd3a6c16ec2b809b7c9c0a3712622
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-12-11 20:12:11 +0000

    buildscript: Initial Import * Use Recommned OPLUS BSP Clang.

---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+function compile() 
+{
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 100G
+export ARCH=arm64
+export KBUILD_BUILD_HOST=MARKxDEVS
+export KBUILD_BUILD_USER="AbzRaider"
+git clone --depth=1 https://github.com/techyminati/android_prebuilts_clang_host_linux-x86_clang-6443078  clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+
+if ! [ -d "out" ]; then
+	echo "Kernel OUT Directory Not Found . Making Again"
+mkdir out
+
+else
+
+	
+	sleep 5
+	echo "out directory already exists , Making Dirty Build !! "
+	echo "If you want to clean Build , just rm -rf out"
+	
+fi
+
+make O=out ARCH=arm64 RM6877_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
+                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+
+function zupload()
+{
+if  [ -d "AnyKernel" ]; then	
+	rm -rf AnyKernel
+fi
+git clone --depth=1 https://github.com/Hunter-commits/anykernel.git AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+zip -r9 TEST-OSS-KERNEL-RM6877.zip *
+curl --upload-file "TEST-OSS-KERNEL-RM6877.zip" https://free.keep.sh
+}
+
+compile
+zupload
